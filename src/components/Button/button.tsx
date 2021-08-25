70471039c6651d3de68984688b9df4476f518020
Wu XiaoQuan <somerq@qq.com>
2021-08-25 18:26:24 +0800

    feat: 初始化button组件

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,7 +1,22 @@
-import { FC } from 'react';
+/**
+ * 
+ * Button组件需求：
+ * 1.不同的按钮类型;
+ * 2.不同的按钮大小;
+ * 3.不可用状态;
+ * 
+ * furthermore:
+ * loading状态
+ * 
+ */
+import React, { FC } from 'react';
+import classNames from 'classnames';
+import { LIB_NAME } from '../../common/constants';
+
+const classPrefix = `${LIB_NAME}-button`;
 
 const Button: FC = () => {
-    return <div></div>
+    return <div>button</div>
 }
 
 export default Button;
