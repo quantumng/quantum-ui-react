d7d90e48ae576eb58d03712b3841d060f63fb8d6
Wu XiaoQuan <somerq@qq.com>
2021-08-26 17:55:50 +0800

    Update 更新Button组件
    更新Button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Button } from './components';
 
@@ -9,7 +8,12 @@ function App() {
       <h1>Hellow World</h1>
       <h2>Hellow World</h2>
       <p>Hello World</p>
-      <Button></Button>
+      <Button>button</Button>
+      <Button btnType="primary">primary</Button>
+      <Button btnType="danger">primary</Button>
+      <Button btnType="link">link</Button>
+      <Button size="large">large</Button>
+      <Button size="small">small</Button>
     </div>
   );
 }
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -13,10 +13,36 @@ import React, { FC } from 'react';
 import classNames from 'classnames';
 import { LIB_NAME } from '../../common/constants';
 
+// enum ButtonType {
+//     default = 'default',
+//     primary = 'primary',
+//     danger = 'danger',
+//     link = 'link'
+// }
+
+// enum ButtonSize {
+//     large = 'large',
+//     small = 'small',
+// }
+
+interface ButtonBaseProps {
+    className?: string,
+    btnType?: 'default' | 'primary' | 'danger' | 'link',
+    size?: 'large' | 'small',
+    disabled?: boolean,
+    href?: string
+}
+
 const classPrefix = `${LIB_NAME}-button`;
 
-const Button: FC = () => {
-    return <div>button</div>
+const Button: FC<ButtonBaseProps> = ({
+    className,
+    href,
+    btnType,
+    children
+}) => {
+    const classname = classNames(classPrefix, className);
+    return btnType === 'link' ? <a className={classname} href={href}>{children}</a> : <button className={classname}>{children}</button>
 }
 
 export default Button;
